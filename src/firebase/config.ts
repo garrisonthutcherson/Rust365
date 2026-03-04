4457e64f29ab05a745f00188e02d060010d4d01d
Garrison Hutcherson <GarrisonTHutcherson@gmail.com>
2026-03-04 07:35:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8156739726-d4da5",
+  "appId": "1:626417827442:web:877b02ec4bb08827380ceb",
+  "apiKey": "AIzaSyAP0oEm-spTkPC9fcjEsrIb4eWYGD682JM",
+  "authDomain": "studio-8156739726-d4da5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626417827442"
+};
